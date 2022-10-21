3eb11dcfeb83cc68b22a0d89a5c90a1d8e8d2393
Raja <111275397+Mr-Raja12@users.noreply.github.com>
2022-10-21 14:40:36 +0530

    Welcome to Employee Wage Programs Using Opps

---
+++ EmpWageBuilder.java
@@ -0,0 +1,6 @@
+package com.bl.empwageprogram;
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Programs Using Opps");
+    }
+}
